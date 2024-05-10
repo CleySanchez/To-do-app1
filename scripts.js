2c4c84a700f0ca6d527fec3f5cc495e1ec51a229
CleySanchez <clement.sanchez.31@gmail.com>
2024-05-10 18:42:55 +0200

    Exercise 1.9 changes
    Press enter to Add to the list.

--- scripts.js
+++ scripts.js
@@ -1,31 +1,50 @@
 $(document).ready(function() {
-    function newItem() {
+  function newItem() {
       let li = $('<li></li>');
       let inputValue = $('#input').val();
       li.text(inputValue);
-  
+
       if (inputValue === '') {
-        alert("You must write something!");
+          alert("You must write something!");
       } else {
-        $('#list').append(li);
+          $('#list').append(li);
       }
-  
+
       li.on("dblclick", function() {
-        li.toggleClass("strike");
+          li.toggleClass("strike");
       });
-  
+
       let crossOutButton = $('<button></button>');
       crossOutButton.text('X');
       crossOutButton.addClass("crossOutButton"); // Add class to the button
       li.append(crossOutButton);
-  
+
       crossOutButton.on("click", function() {
-        li.addClass("delete");
+          li.addClass("delete");
       });
-  
+
       $('#list').sortable();
-    }
-  
-    $('#button').on('click', newItem);
+  }
+
+  $('#button').on('click', newItem);
+
+  // Add event listener for the Enter key press
+  $('#input').keypress(function(event) {
+      // Check if the Enter key is pressed
+      if (event.which === 13) {
+          // Prevent default action (form submission)
+          event.preventDefault();
+          // Call the newItem function to add a new item
+          newItem();
+      }
   });
-  
+
+  // Prevent page reload on Enter key press
+  $(document).keypress(function(event) {
+      // Check if the Enter key is pressed
+      if (event.which === 13) {
+          // Prevent default action (form submission)
+          event.preventDefault();
+      }
+  });
+});
